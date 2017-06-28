652a110b4ecb1578f1e35937e92e8bd4dc4240f4
rfyiambest <rfyiambest@163.com>
2017-06-28 23:10:28 +0800

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='pyLruCache',
+      version='3.0.1',
+      description='Use Lru dict for cache.',
+      url='http://github.com/rfyiamcool',
+      author='ruifengyun',
+      author_email='rfyiamcool@163.com',
+      long_description=open('README.md').read(),
+      license='MIT',
+      packages=['pyLruCache'],
+      keywords = "lru cache author ruifengyun",
+      classifiers = [
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License'
+          ]
+      
+      )
